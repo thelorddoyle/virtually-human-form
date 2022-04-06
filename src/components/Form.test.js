2d09c37b27b01d1ba0f345e37b09b54d88bcf5b0
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-04-06 18:28:09 +1000

    moved button testing to button folder

--- src/components/Form.test.js
+++ src/components/Form.test.js
@@ -43,19 +43,3 @@ describe('My form input rendering tests', () => {
         expect(imageUploadPrompt).toBeInTheDocument();
     })
 })
-
-describe('My button rendering tests', () => {
-
-    it('will render the buttons with the correct default values', () => {
-        setupTest()
-
-        const removeButton = screen.getByRole('button', { name: /remove/i })
-        expect(removeButton).toBeInTheDocument()
-
-        const saveChangesButton = screen.getByRole('button', { name: /save changes/i })
-        expect(saveChangesButton).toBeInTheDocument()
-
-        const discardButton = screen.getByRole('button', { name: /discard/i })
-        expect(discardButton).toBeInTheDocument()
-    })
-})
---
+++ src/components/formComponents/Buttons/Buttons.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable testing-library/no-debugging-utils */
+import { render, screen } from "@testing-library/react";
+import { noErrorsAllowed } from '../../../test-utils';
+
+import Form from "./../../Form";
+
+// allows me to call this function and render form in all it blocks
+const setupTest = () => {render(<Form />);};
+
+noErrorsAllowed();
+
+describe('My button rendering tests', () => {
+    it('will render the buttons with the correct default values', () => {
+        setupTest()
+
+        const removeButton = screen.getByRole('button', { name: /remove/i })
+        expect(removeButton).toBeInTheDocument()
+
+        const saveChangesButton = screen.getByRole('button', { name: /save changes/i })
+        expect(saveChangesButton).toBeInTheDocument()
+
+        const discardButton = screen.getByRole('button', { name: /discard/i })
+        expect(discardButton).toBeInTheDocument()
+    })
+})
